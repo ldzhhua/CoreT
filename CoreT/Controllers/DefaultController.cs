2566383875cebdbc98998bd7319050b8dbabf40e
ldzhhua <ldzh@vip.qq.com>
2020-02-25 13:10:22 +0800

    添加项目文件。

---
+++ CoreT/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        /// <summary>
+        /// 这是一个api方法的注释
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Getid(int id)
+        {
+            return $"你请求的 id 是 {id}";
+        }
+    }
+}
